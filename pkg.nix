2f2e5a0b684f886a7585161d30e8cda962c7eefb
Griffin Smith <root@gws.fyi>
2019-11-27 13:20:46 -0500

    Gitignore source before passing to nix
    Call hercules-ci's gitignoreSource on the src path before passing to
    nix, which both prevents spurious rebuilds and also makes compilation
    via `nix build` (which under the hood uses cabal v1-build) work while
    also doing development using `cabal new-build`

--- pkg.nix
+++ pkg.nix
@@ -1,7 +1,19 @@
-{ nixpkgs ? import ./nixpkgs.nix {} }:
-let inherit (nixpkgs) pkgs; in
+{ nixpkgs ? import ./nixpkgs.nix {}
+,
+}:
+let
+  inherit (builtins) filterSource elem not;
+  inherit (nixpkgs) pkgs;
+  gitignoreSource = (import (pkgs.fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "gitignore";
+    rev = "f9e996052b5af4032fe6150bba4a6fe4f7b9d698";
+    sha256 = "0jrh5ghisaqdd0vldbywags20m2cxpkbbk5jjjmwaw0gr8nhsafv";
+    # date = 2019-09-18T15:15:15+02:00;
+  }) { inherit (pkgs) lib; }).gitignoreSource;
+in
 import (pkgs.haskellPackages.haskellSrc2nix {
   name = "xanthous";
-  src = ./.;
+  src = gitignoreSource ./.;
   extraCabal2nixOptions = "--hpack";
 })
