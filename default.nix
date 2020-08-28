9e02457bb2c3fde17bba0aa573e9e9c937f4f906
Griffin Smith <grfn@gws.fyi>
2020-08-28 10:25:10 -0400

    revert(glittershark): "Temporarily skip grfn's subtree"
    This reverts commit 475d41f698415919093ca98f676ffcda218093ac. I'd like
    my derivations back, please.
    
    Changes necessary to get this working:
    
    - Don't depend on `nixpkgs` being in the NIX_PATH for my website - it's
      not necessary anyway since emacs 27 is mainline now
    - .skip-subtrees on things that shouldn't be evaluated anyway
    - Get rid of system/pkgs, and move the one thing in there that *wasn't*
      already in third_party (alsi) to third_party
    - Drop notifymuch for now - it's not working, and I'll probably get it
      landed in nixpkgs before I manage to get it working
    - Add __readTree = true to my systems so they get built.
    - explicitly disable ci for xanthous, which is failing to build and had
      been omitted previously
    
    Change-Id: I20f5e81d6eb7ffe040091a08d75d0cb15304f707
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/1864
    Tested-by: BuildkiteCI
    Reviewed-by: tazjin <mail@tazj.in>

--- default.nix
+++ default.nix
@@ -2,6 +2,9 @@
 , lib ? pkgs.lib
 , ...
 }:
-pkgs.haskell.lib.failOnAllWarnings (
+(pkgs.haskell.lib.failOnAllWarnings (
   pkgs.haskellPackages.callPackage (import ./pkg.nix { inherit pkgs; }) {}
-)
+)) // {
+  # TODO(grfn): Get this passing (see https://buildkite.com/tvl/depot/builds/3055)
+  meta.ci = false;
+}
