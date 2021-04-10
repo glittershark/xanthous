f565e98ae7dbc172442eec7444a524df66e8589b
sterni <sternenseemann@systemli.org>
2021-04-10 23:30:09 +0200

    refactor(users/grfn): build all haskell packages without ifd
    Since we need to override random to 1.2.0 globally for xanthous
    (otherwise propagation causes two versions of random to be propagated
    for xanthous) evaluating haskell related stuff using import from
    derivation can be very expensive since utilities like hpack and
    cabal2nix need to be built for that. This means that for every channel
    bump we potentially need to do a world rebuild of haskellPackages first.
    
    To solve this we check in the cabal2nix-generated nix expressions for
    owothia and xanthous.
    
    Change-Id: I8fff70b4b6c303d1920f8bcac53520a09999b206
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/2921
    Tested-by: BuildkiteCI
    Reviewed-by: grfn <grfn@gws.fyi>
    Reviewed-by: tazjin <mail@tazj.in>

--- pkg.nix
+++ pkg.nix
@@ -1,16 +1,79 @@
-{ depot ? (import ../../../. {})
-, pkgs ? depot.third_party.nixpkgs
-, ... }:
-
-let
-  ignore = depot.third_party.gitignoreSource.gitignoreFilter ./.;
-in import (pkgs.haskellPackages.haskellSrc2nix {
-  name = "xanthous";
-  src = builtins.path {
-    name = "xanthous-source";
-    path = ./.;
-    filter = path: type: ignore path type
-      || builtins.baseNameOf path == "package.yaml";
-  };
-  extraCabal2nixOptions = "--hpack";
-})
+{ mkDerivation, aeson, array, async, base, bifunctors, brick
+, checkers, classy-prelude, comonad, comonad-extras, constraints
+, containers, criterion, data-default, deepseq, directory, fgl
+, fgl-arbitrary, file-embed, filepath, generic-arbitrary
+, generic-lens, generic-monoid, groups, hgeometry
+, hgeometry-combinatorial, hpack, JuicyPixels, lens
+, lens-properties, lib, lifted-async, linear, megaparsec, mmorph
+, monad-control, MonadRandom, mtl, optparse-applicative, parallel
+, parser-combinators, pointed, QuickCheck, quickcheck-instances
+, quickcheck-text, random, random-extras, random-fu, random-source
+, Rasterific, raw-strings-qq, reflection, semigroupoids, splitmix
+, stache, streams, tasty, tasty-hunit, tasty-quickcheck, text
+, text-zipper, tomland, transformers, vector, vty, witherable, yaml
+, zlib
+}:
+mkDerivation {
+  pname = "xanthous";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson array async base bifunctors brick checkers classy-prelude
+    comonad comonad-extras constraints containers criterion
+    data-default deepseq directory fgl fgl-arbitrary file-embed
+    filepath generic-arbitrary generic-lens generic-monoid groups
+    hgeometry hgeometry-combinatorial JuicyPixels lens lifted-async
+    linear megaparsec mmorph monad-control MonadRandom mtl
+    optparse-applicative parallel parser-combinators pointed QuickCheck
+    quickcheck-instances quickcheck-text random random-extras random-fu
+    random-source Rasterific raw-strings-qq reflection semigroupoids
+    splitmix stache streams text text-zipper tomland transformers
+    vector vty witherable yaml zlib
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    aeson array async base bifunctors brick checkers classy-prelude
+    comonad comonad-extras constraints containers criterion
+    data-default deepseq directory fgl fgl-arbitrary file-embed
+    filepath generic-arbitrary generic-lens generic-monoid groups
+    hgeometry hgeometry-combinatorial JuicyPixels lens lifted-async
+    linear megaparsec mmorph monad-control MonadRandom mtl
+    optparse-applicative parallel parser-combinators pointed QuickCheck
+    quickcheck-instances quickcheck-text random random-extras random-fu
+    random-source Rasterific raw-strings-qq reflection semigroupoids
+    splitmix stache streams text text-zipper tomland transformers
+    vector vty witherable yaml zlib
+  ];
+  testHaskellDepends = [
+    aeson array async base bifunctors brick checkers classy-prelude
+    comonad comonad-extras constraints containers criterion
+    data-default deepseq directory fgl fgl-arbitrary file-embed
+    filepath generic-arbitrary generic-lens generic-monoid groups
+    hgeometry hgeometry-combinatorial JuicyPixels lens lens-properties
+    lifted-async linear megaparsec mmorph monad-control MonadRandom mtl
+    optparse-applicative parallel parser-combinators pointed QuickCheck
+    quickcheck-instances quickcheck-text random random-extras random-fu
+    random-source Rasterific raw-strings-qq reflection semigroupoids
+    splitmix stache streams tasty tasty-hunit tasty-quickcheck text
+    text-zipper tomland transformers vector vty witherable yaml zlib
+  ];
+  benchmarkHaskellDepends = [
+    aeson array async base bifunctors brick checkers classy-prelude
+    comonad comonad-extras constraints containers criterion
+    data-default deepseq directory fgl fgl-arbitrary file-embed
+    filepath generic-arbitrary generic-lens generic-monoid groups
+    hgeometry hgeometry-combinatorial JuicyPixels lens lifted-async
+    linear megaparsec mmorph monad-control MonadRandom mtl
+    optparse-applicative parallel parser-combinators pointed QuickCheck
+    quickcheck-instances quickcheck-text random random-extras random-fu
+    random-source Rasterific raw-strings-qq reflection semigroupoids
+    splitmix stache streams text text-zipper tomland transformers
+    vector vty witherable yaml zlib
+  ];
+  prePatch = "hpack";
+  homepage = "https://github.com/glittershark/xanthous#readme";
+  description = "A WIP TUI RPG";
+  license = lib.licenses.gpl3Only;
+}
