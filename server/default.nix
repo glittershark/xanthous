3c25542bbdaf1376da232a135f518444fe8f66c5
Vincent Ambo <mail@tazj.in>
2023-07-14 00:12:01 +0300

    feat(nix/sparseTree): add optional `name` argument
    This allows pinning the name of the sparse tree derivation, which
    stops the continous rebuilding of tvix-store-proto dependents.
    
    I've opted to let the function take an attribute set instead and
    refactored the call sites appropriately.
    
    Change-Id: I3e57785094b1adbfffa24caf9f1c3384844fa200
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/8965
    Reviewed-by: grfn <grfn@gws.fyi>
    Tested-by: BuildkiteCI
    Reviewed-by: sterni <sternenseemann@systemli.org>

--- server/default.nix
+++ server/default.nix
@@ -10,10 +10,13 @@ depot.third_party.naersk.buildPackage {
 
   # Workaround for a potential Nix bug related to restricted eval.
   # See https://github.com/nix-community/naersk/issues/169
-  root = depot.nix.sparseTree ./. [
-    ./Cargo.toml
-    ./Cargo.lock
-  ];
+  root = depot.nix.sparseTree {
+    root = ./.;
+    paths = [
+      ./Cargo.toml
+      ./Cargo.lock
+    ];
+  };
 
   passthru = {
     docker = import ./docker.nix args;
